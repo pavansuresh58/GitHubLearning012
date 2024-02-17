70570b235230cd2f2380616751cdf3a720db5a00
kuna suresh <pavansuresh58@gmail.com>
2024-02-17 14:42:56 +0530

    05-01-2024

--- src/test/java/testPackage/Test2.java
+++ src/test/java/testPackage/Test2.java
@@ -2,8 +2,9 @@ package testPackage;
 
 public class Test2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	{
+		System.out.println("This is new Test file for Git");
 
 	}
 
